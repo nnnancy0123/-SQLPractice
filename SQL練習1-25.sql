46ce5205b3e087e8cca84a066bceeb3786f89735
Jin <106419088+nnnancy0123@users.noreply.github.com>
2022-12-03 19:23:50 +0900

    1-25
    1-25

--- SQL練習1-25.sql
+++ SQL練習1-25.sql
@@ -8,23 +8,36 @@ from
 group by
   group_name 
 order by
-  group_name ASC
-  --意外と勘違いしやすいポイントとして、MIN()関数を使った方がランキング最上位、MAX()関数を使った方がランキング最下位となりますので注意
-  
+  group_name ASC                                  
+--意外と勘違いしやすいポイントとして、MIN()関数を使った方がランキング最上位、MAX()関数を使った方がランキング最下位となりますので注意
+
 --2問題：各国の平均身長を高い方から順に表示してください。ただし、FROM句はcountriesテーブルとしてください。
-select * from countries
+select
+  c.name as 国名
+  , avg(p.height) as 平均身長 
+from
+  countries c 
+  left join players p 
+    on c.id = p.country_id 
+group by
+  c.name 
+order by avg(p.height) desc
+  
+--70問題：1980年生まれと、1981年生まれの選手が何人いるか調べてください。ただし、日付関数は使用せず、UNION句を使用してください。                                         
+  select
+  '1980' as 誕生年
+  , count(id) 
+from
+  players 
+WHERE
+  birth BETWEEN '1980-1-1' AND '1980-12-31' 
+union 
+select
+  '1981' as 誕生年
+  , count(id) 
+from
+  players 
+WHERE
+  birth BETWEEN '1981-1-1' AND '1981-12-31'      
+ --回答：‘描述'，是死的。用以描述BETWEEN '1981-1-1' AND '1981-12-31'?个方位的?
 
-select * from players
-
-
-
---70問題：1980年生まれと、1981年生まれの選手が何人いるか調べてください。ただし、日付関数は使用せず、UNION句を使用してください。
-
-select '1980'as 誕生年, count(id) 
-from players 
-WHERE birth BETWEEN '1980-1-1' AND '1980-12-31'
-union
-select '1981'as 誕生年, count(id) 
-from players 
-WHERE birth BETWEEN '1981-1-1' AND '1981-12-31'
---回答：‘描述'，是死的。用以描述BETWEEN '1981-1-1' AND '1981-12-31'?个方位的?
